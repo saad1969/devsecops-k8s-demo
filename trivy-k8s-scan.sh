a13fb9917fb9b1861ed1451c831926c3ed0216b8
SAAD <saad1969@gmail.com>
2022-05-20 16:26:30 +0200

    Update trivy-k8s-scan.sh

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -3,10 +3,10 @@
 
 echo $imageName #getting Image name from env variable
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:latest -q image --exit-code 0 --severity LOW,MEDIUM,HIGH $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:latest -q image --exit-code 1 --severity CRITICAL $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
-    # Trivy scan result processing
+    # Trivy scan result processing 
     exit_code=$?
     echo "Exit Code : $exit_code"
 
